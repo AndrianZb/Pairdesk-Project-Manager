01652ab6cacf8b6d713a7044c52ca9459964f614
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-11-29 15:49:09 -0500

    initial commit

---
+++ src/main/java/com/pairdesk/pairdeskmanagement/PairdeskmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.pairdesk.pairdeskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairdeskmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairdeskmanagementApplication.class, args);
+    }
+
+}
